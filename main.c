db6407da7ec23a0086a7b8ab42b1fa076bbc5a62
nikov-afk <kallash.nniikov@gmail.com>
2026-07-16 15:33:56 -0300

    adicionado inicializacao da matriz

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+typedef struct {
+  int eh_bomba;
+  int esta_aberta;
+  int vizinhos;
+} Celula;
+
+// variaveis globais
+Celula jogo[10][10];
+int l, c, tam = 10;
+
+// procedimento para inicializar a matriz do jogo
+void inicializar_jogo() {
+  for (l = 0; l < tam; l++) {
+    for (c = 0; c < tam; c++) {
+      jogo[l][c].eh_bomba = 0;
+      jogo[l][c].esta_aberta = 0;
+      jogo[l][c].vizinhos = 0;
+    }
+  }
+}
+
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+  printf("Hello, World!\n");
+  return 0;
 }
